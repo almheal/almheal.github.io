1e04eee760c4b12635d8e0f2a3de796fa54bf2e7
dmitri <yellowblackyellow123@gmail.com>
2021-02-24 00:34:18 +0800

    init shop

---
+++ shop/js/index.js
@@ -0,0 +1,31 @@
+//intro slider
+$('.intro__slider').slick({
+  prevArrow: $('.intro-button-prev'),
+  nextArrow: $('.intro-button-next'),
+  dots: false,
+  fade: true,
+  cssEase: 'linear',
+  lazyLoad: 'ondemand',
+  draggable: false,
+})
+
+//sizes product slider
+$('.sizes__slider').slick({
+  dots: false,
+  cssEase: 'linear',
+  speed: 700,
+  useCSS: false,
+  prevArrow: $('.sizes-button-prev'),
+  nextArrow: $('.sizes-button-next'),
+})
+
+//specification slider
+$('.specification__slider').slick({
+  dots: false,
+  useCSS: false,
+  speed: 800,
+  autoplay: true,
+  variableWidth: true,
+  prevArrow: $('.specification-button-prev'),
+  nextArrow: $('.specification-button-next'),
+})
