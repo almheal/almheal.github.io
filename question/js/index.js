62d741b94df6ab3e0e6042942394cac11549b885
dmitri <yellowblackyellow123@gmail.com>
2021-02-28 06:11:28 +0800

    init question

--- question/js/index.js
+++ question/js/index.js
@@ -1,83 +1,95 @@
-const nextButton = document.querySelector('.next')
-const prevButton = document.querySelector('.prev')
-const endButton = document.querySelector('.end')
-const stepper = document.querySelector('.stepper__body')
-const progressBar = document.querySelector('.progress__bar')
-const steps = document.querySelectorAll('[data-step]')
 
-stepper.addEventListener('click', clickOnStepper)
 
-function clickOnStepper(e) {
-  if (e.target.dataset.next) {
-    nextStep()
+class Stepper {
+  constructor({
+    stepsSelector,
+    selectorProgress,
+    prevButton,
+    nextButton,
+    numberSelector,
+  }) {
+    this.steps = [...document.querySelectorAll(stepsSelector)]
+    this.progress = document.querySelector(selectorProgress)
+    this.prevButton = document.querySelector(prevButton)
+    this.nextButton = document.querySelector(nextButton)
+    this.number = document.querySelector(numberSelector)
+    this.lengthSteps = this.steps.length
+    this.oneNumberStep = parseInt(100 / this.steps.length)
   }
-  if (e.target.dataset.prev) {
-    prevStep()
-  }
-}
 
-function nextStep() {
-  const activeStep = stepper.querySelector('[data-step].active')
-  if (
-    !activeStep.nextElementSibling ||
-    !activeStep.nextElementSibling.dataset.step
-  ) return
+  searchActiveStep() {
+    const [activeStep] = this.steps.filter((item) =>
+      item.classList.contains('active')
+    )
+    return activeStep
+  }
 
-  const nextElement = activeStep.nextElementSibling
-  nextElement.classList.add('active')
-  if (!prevButton.classList.contains('active')) {
-    prevButton.classList.add('active')
+  addClass(el, className) {
+    el.classList.add(className)
   }
-  activeStep.classList.remove('active')
-  if(!nextElement.previousElementSibling || !nextElement.nextElementSibling.dataset.step){
-    endButton.classList.add('active')
-    nextButton.classList.remove('active')
-  }
-  progress('next')
-}
 
-function prevStep() {
-  const activeStep = stepper.querySelector('[data-step].active')
-  const prevStep = activeStep.previousElementSibling
-  if (!prevStep || !prevStep.dataset.step) {
-    prevButton.classList.remove('active')
-    return
-  }
-  prevStep.classList.add('active')
-  activeStep.classList.remove('active')
-  if (
-    !prevStep.previousElementSibling ||
-    !prevStep.previousElementSibling.dataset.step
-  ) {
-    prevButton.classList.remove('active')
-  }
-  if (!nextButton.classList.contains('active')) {
-    nextButton.classList.add('active')
-    endButton.classList.remove('active')
-  }
-  progress('prev')
-}
-
-function progress(operation) {
-  const allStepsLength = steps.length
-  const numberOneStep = Math.ceil(100 / allStepsLength)
-  if (operation === 'prev') {
-    const actualWidth = parseInt(progressBar.style.width)
-    const width = actualWidth - numberOneStep
-    progressBar.style.width = width + '%'
-    return
-  }
-  let prevStepsNumber = 0
-  for (let i = 0; i < allStepsLength; i++) {
-    if (steps[i].classList.contains('active')) {
-      break
-    } else {
-      prevStepsNumber++
+  prevStep() {
+    const activeStep = this.searchActiveStep()
+    activeStep.classList.remove('active')
+    this.addClass(activeStep.previousElementSibling, 'active')
+    if (Number(activeStep.dataset.step) === 2) {
+      this.prevButton.classList.remove('active')
     }
+    this.calculateProgress('prev')
   }
-  let width
-  if (operation === 'next') {
-    width = prevStepsNumber * numberOneStep
-    progressBar.style.width = width + '%'
+
+  nextStep() {
+    if (!this.prevButton.classList.contains('active')) {
+      this.addClass(this.prevButton, 'active')
+    }
+
+    const activeStep = this.searchActiveStep()
+    const stepNumber = Number(activeStep.dataset.step)
+
+    if (stepNumber === this.steps.length) return
+      activeStep.classList.remove('active')
+      this.addClass(activeStep.nextElementSibling, 'active')
+      this.calculateProgress('next')
+  }
+
+  calculateProgress(operation) {
+    if (operation === 'prev') {
+      const updateProgress =
+        parseInt(this.progress.style.width) - this.oneNumberStep
+      this.progress.style.width = updateProgress + '%'
+      this.number.textContent = updateProgress + '%'
+      return
+    }
+    const activeStep = this.searchActiveStep()
+
+    let prevSteps = 0
+    for (let i = 0; i < Number(activeStep.dataset.step); i++) {
+      prevSteps++
+    }
+    const width = parseInt(prevSteps * this.oneNumberStep) + '%'
+    this.number.textContent = width
+    this.progress.style.width = width
+  }
+
+  initListeners() {
+    this.prevButton.addEventListener('click', this.prevStep.bind(this))
+    this.nextButton.addEventListener('click', this.nextStep.bind(this))
+  }
+
+  init() {
+    this.initListeners()
+    this.progress.style.width = this.oneNumberStep + '%'
+    this.number.textContent = this.oneNumberStep + '%'
   }
 }
+;
+
+const stepper = new Stepper({
+  stepsSelector: '[data-step]',
+  selectorProgress: '[data-progress]',
+  prevButton: '[data-prev]',
+  nextButton: '[data-next]',
+  numberSelector: '[data-number]',
+})
+
+stepper.init()
